c7e52a539511b272e361b47b1d05ce044644be49
emi <bell881122@gmail.com>
2022-08-14 21:18:04 +0800

    add document

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="zh-TW">
+      <Head>
+        <title>笑笑的程式人生</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
