b727df20fe84c8b5af44e4d4e22dd8add05cceb8
emi <bell881122@gmail.com>
2022-08-14 17:16:02 +0800

    調整 BaseLayout 連結

--- layouts/BaseLayout.tsx
+++ layouts/BaseLayout.tsx
@@ -1,5 +1,4 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+/* eslint-disable @next/next/no-html-link-for-pages */
 import { useState } from 'react';
 import { contextList } from 'config/config';
 import styles from './BaseLayout.module.scss';
@@ -9,10 +8,8 @@ type Props = {
 };
 
 const BaseLayout = ({ children }: Props) => {
-  const router = useRouter();
   const [showMenu, setShowMenu] = useState(false);
   const handleShowMenu = () => setShowMenu(state => !state);
-  const handleLink = (url: string) => router.push(url);
 
   return (
     <div className='relative bg-gray-100 min-h-screen'>
@@ -21,23 +18,27 @@ const BaseLayout = ({ children }: Props) => {
           <div className='fixed top-0 left-0 w-full min-h-screen bg-[rgba(0,0,0,0.5)]' />
           <div className="fixed mt-10 w-full bg-gray-100">
             {contextList.map(item =>
-              <div key={item} onClick={() => handleLink(`/post/${item}`)} className="mx-4 p-2 border-b cursor-pointer hover:text-gray-800 text-gray-500">
+              <a key={item} href={`/post/${item}`} className="block mx-4 p-2 border-b cursor-pointer hover:text-gray-800 text-gray-500">
                 {item}
-              </div>
+              </a>
             )}
           </div>
         </div>
       }
       <div className="h-10 border-[rgb(8,145,178,0.3)] bg-white w-full fixed shadow-xl">
         <div className='py-[5px] px-4 flex items-stretch sm:items-baseline max-w-[1000px] mx-auto'>
-          <Link href="/">
-            <h1 className='mr-auto font-bold text-[20px] text-cyan-700 cursor-pointer text-ellipsis overflow-hidden whitespace-nowrap'>笑笑的程式人生</h1>
-          </Link>
+          <div className="mr-auto">
+            <a href="/">
+              <h1 className='mr-auto font-bold text-[20px] text-cyan-700 text-ellipsis overflow-hidden whitespace-nowrap'>笑笑的程式人生</h1>
+            </a>
+          </div>
           <div className="hidden sm:flex">
             {contextList.map(item =>
-              <span key={item} onClick={() => handleLink(`/post/${item}`)} className="block ml-4 cursor-pointer text-gray-500">
-                {item}
-              </span>
+              <a key={item} href={`/post/${item}`}>
+                <span className="block ml-4 text-gray-500">
+                  {item}
+                </span>
+              </a>
             )}
           </div>
           <div onClick={() => handleShowMenu()} className="sm:hidden w-6 py-[6px] cursor-pointer">
@@ -50,13 +51,13 @@ const BaseLayout = ({ children }: Props) => {
       </div>
       <div className="h-8 bg-cyan-700 w-full flex ">
         <small className='my-auto px-4 max-w-[1000px] mx-auto text-gray-300 text-ellipsis overflow-hidden whitespace-nowrap'>
-          <span
-            className='text-white font-bold mr-4 cursor-pointer'
-            onClick={() => handleLink("/about-me")}
-          >關於我</span>
           <a
-            className='text-white font-bold mr-4'
-            href="https://github.com/bell881122"
+            href="/about-me"
+            className='text-white font-bold mr-3'
+          >關於我</a>
+          <a
+            className='text-white font-bold mr-3'
+            href="https://github.com/bell881122/markdown-blog"
             target="_blank"
             rel="noreferrer"
           >Github</a>
