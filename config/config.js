b962c9404a44807e4d3410ddc5e8c5f33f832c32
Emi <emi_chang@lhwlfw.com>
2022-08-17 14:12:07 +0800

    更換文章預設圖片

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-export const defaultImage = "https://images.unsplash.com/photo-1657477612554-ff8f28dd15e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
+export const defaultImage = "https://i.imgur.com/N3DIth6.jpg"; // origin: https://stocksnap.io/photo/macbook-computer-6UHHE19YG7
 export const contextList = ['JavaScript', 'CSS', 'Web', 'Develop'];
