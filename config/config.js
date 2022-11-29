cc486e72902448163a7a66f6a7396777403d438e
emi <bell881122@gmail.com>
2022-11-29 19:41:29 +0800

    新增 YDKJS 預設圖片

--- config/config.js
+++ config/config.js
@@ -4,6 +4,8 @@ export const defaultImage = {
     default: "https://cdn.pixabay.com/photo/2019/10/03/12/12/javascript-4523100_960_720.jpg",
     // https://unsplash.com/photos/xkBaqlcqeb4
     React: "https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    // https://pixabay.com/vectors/javascript-js-logo-source-code-736400/
+    YDKJS: "https://imgur.com/YblE6ct.jpg",
     // https://pixabay.com/vectors/node-js-logo-nodejs-javascript-736399/
     Nodejs: "https://i.imgur.com/O73v0MV.jpg",
   },
