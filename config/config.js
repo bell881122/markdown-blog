9026944caf17c1c484c3051788276e67eee5513a
emi <bell881122@gmail.com>
2022-11-30 17:16:20 +0800

    Develop 目錄改為 Dev

--- config/config.js
+++ config/config.js
@@ -1,2 +1,29 @@
-export const defaultImage = "https://images.unsplash.com/photo-1657477612554-ff8f28dd15e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
-export const contextList = ['JavaScript', 'CSS', 'Web', 'Develop'];
+export const defaultImage = {
+  JavaScript: {
+    // https://pixabay.com/photos/javascript-programmer-code-4523100/
+    default: "https://cdn.pixabay.com/photo/2019/10/03/12/12/javascript-4523100_960_720.jpg",
+    // https://unsplash.com/photos/xkBaqlcqeb4
+    React: "https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    // https://pixabay.com/vectors/javascript-js-logo-source-code-736400/
+    YDKJS: "https://imgur.com/YblE6ct.jpg",
+    // https://pixabay.com/vectors/node-js-logo-nodejs-javascript-736399/
+    Nodejs: "https://i.imgur.com/O73v0MV.jpg",
+  },
+  CSS: {
+    // https://unsplash.com/photos/_yMciiStJyY
+    default: "https://images.unsplash.com/photo-1517134191118-9d595e4c8c2b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  Web: {
+    // https://unsplash.com/photos/EhTcC9sYXsw
+    default: "https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  Dev: {
+    // https://unsplash.com/photos/DtDlVpy-vvQ
+    default: "https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
+  },
+  common: {
+    // https://stocksnap.io/photo/macbook-computer-6UHHE19YG7
+    default: "https://i.imgur.com/N3DIth6.jpg",
+  },
+}
+export const contextList = Object.keys(defaultImage);
